53576952ddee1b6c758b0b82d70ee872e4a2b607
mosheilan942 <moisi6510@gmail.com>
2024-01-25 16:54:27 +0200

    add sh file

---
+++ server/startDB.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Start Redis
+sudo service redis-server start
+# Start MongoDB
+sudo systemctl start mongod
+# Start PostgreSQL
+sudo service postgresql start
+echo "All services started!"
