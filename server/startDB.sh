8c5d081c88a00e44050cd2a5a4140a13bdd18392
mosheilan942 <moisi6510@gmail.com>
2024-01-28 10:49:04 +0200

    add postgrsql con

--- server/startDB.sh
+++ server/startDB.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 # Start Redis
 sudo service redis-server start
+
+# Stop Redis
+sudo service redis-server stop
+
+
 # Start MongoDB
 sudo systemctl start mongod
+
+# Stop MongoDB
+sudo systemctl stop mongod
+
+
 # Start PostgreSQL
 sudo service postgresql start
+
+# Stop PostgreSQL
+sudo systemctl stop postgresql
+
 echo "All services started!"
 
