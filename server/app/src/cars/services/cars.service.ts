cc430b8a615d296e900941e524f01552ff4e505c
mosheilan942 <moisi6510@gmail.com>
2024-02-06 10:09:45 +0200

    save

--- server/app/src/cars/services/cars.service.ts
+++ server/app/src/cars/services/cars.service.ts
@@ -5,33 +5,29 @@ import { CreateCarDto } from '../dto/create-car.dto';
 import { UpdateCarDto } from '../dto/update-car.dto';
 import { Car } from '../entities/car.entity.mongodb';
 
-
 @Injectable()
 export class CarsService {
-  constructor(@InjectModel(Car.name) private readonly carModel: Model<Car>, 
-  ) {}
+  constructor(@InjectModel(Car.name) private readonly carModel: Model<Car>) {}
 
   async create(createCarDto: CreateCarDto): Promise<Car | void> {
-    const car: Car = { ...createCarDto }
+    const car: Car = { ...createCarDto };
     const res = await this.carModel.create(car);
     return res;
   }
 
   async findAll() {
     const res = this.carModel.find().exec();
-    return res
+    return res;
   }
 
   async findOne(id: string): Promise<string | Car> {
     const res = await this.carModel.findOne({ _id: id }).exec();
-    if (res) return res
-    return "error"
+    if (res) return res;
+    return 'error';
   }
 
   async delete(id: string) {
-    const deletedCar = await this.carModel
-      .findOneAndDelete({ _id: id })
-      .exec();
+    const deletedCar = await this.carModel.findOneAndDelete({ _id: id }).exec();
     return deletedCar;
   }
 }
